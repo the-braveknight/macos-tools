3357e2d0d339c28e94cb32bb26caa208f8a9660a
Zaid Rahawi <zaid@Zaids-macOS.local>
2017-12-11 18:28:56 +0300

    Add option for macOS version checking in settings.plist

--- download.sh
+++ download.sh
@@ -13,13 +13,25 @@ function download() {
     curl --remote-name --progress-bar --location https://bitbucket.org/$scrape
 }
 
+os_version=$(./os_version.sh)
+
 function downloadCategory() {
     for ((file=0; 1; file++)); do
-        author=$(/usr/libexec/PlistBuddy -c "Print :Downloads:$1:$file:author" $settings 2>&1)
-        name=$(/usr/libexec/PlistBuddy -c "Print :Downloads:$1:$file:name" $settings 2>&1)
+        author=$(/usr/libexec/PlistBuddy -c "Print ':Downloads:$1:$file:author'" $settings 2>&1)
+        name=$(/usr/libexec/PlistBuddy -c "Print ':Downloads:$1:$file:name'" $settings 2>&1)
 
         if [[ "$author" == *"Does Not Exist"* ]]; then
             break
+        fi
+
+        minimum_os=$(/usr/libexec/PlistBuddy -c "Print ':Downloads:$1:$file:Minimum OS'" $settings 2>&1)
+        if [[ "$minimum_os" != *"Does Not Exist"* && $os_version -lt $minimum_os ]]; then
+            continue
+        fi
+
+        maximum_os=$(/usr/libexec/PlistBuddy -c "Print ':Downloads:$1:$file:Maximum OS'" $settings 2>&1)
+        if [[ "$maximum_os" != *"Does Not Exist"* && $os_version -gt $maximum_os ]]; then
+            continue
         fi
 
         download $author $name
