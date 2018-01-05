9dc7fa09c11578d07418804fdb5b0765649d1980
Zaid Rahawi <zaid.riadh@gmail.com>
2018-01-05 21:39:44 +0300

    Merge install_config.sh & update_config.sh scripts

--- install_config.sh
+++ install_config.sh
@@ -2,13 +2,68 @@
 
 DIR=$(dirname $0)
 
-if [[ ! -e $1 ]]; then
-    echo "Usage: install_config.sh {Clover config.plist}"
-    echo "Example: install_config.sh ~/Desktop/config.plist"
-    exit 1
-fi
-
 EFI=$($DIR/mount_efi.sh)
+current_plist=$EFI/EFI/Clover/config.plist
 
-echo Copying $1 to $EFI/EFI/Clover
-cp $1 $EFI/EFI/Clover
+function showOptions() {
+    echo "-u,  Update only relevant settings. (Preserve user settings like SMBIOS)"
+    echo "-h,  Show this help message."
+    echo "Usage: $(basename $0) [Options] [New config.plist]"
+    echo "Example: $(basename $0) -u ~/Downloads/config.plist"
+}
+
+function replaceVar() {
+# $1: Property key
+# $2: New config.plist
+    value=$(/usr/libexec/plistbuddy -c "Print '$1'" $2)
+    /usr/libexec/plistbuddy -c "Set '$1' '$value'" $current_plist
+}
+
+function replaceDict() {
+# $1: Property key
+# $2: New config.plist
+    /usr/libexec/plistbuddy -x -c "Print '$1'" $2 > /tmp/org_rehabman_node.plist
+    /usr/libexec/plistbuddy -c "Delete '$1'" $current_plist
+    /usr/libexec/plistbuddy -c "Add '$1' dict" $current_plist
+    /usr/libexec/plistbuddy -c "Merge /tmp/org_rehabman_node.plist '$1'" $current_plist
+}
+
+function replaceConfig() {
+# $1: New config.plist
+    echo Copying $1 to $current_plist
+    cp $1 $current_plist
+}
+
+function updateConfig() {
+# $1: New config.plist
+    echo Updating config.plist at $current_plist
+    replaceDict ":ACPI" $1
+    replaceDict ":Boot" $1
+    replaceDict ":Devices" $1
+    replaceDict ":KernelAndKextPatches" $1
+    replaceDict ":SystemParameters" $1
+    replaceVar ":RtVariables:BooterConfig" $1
+    replaceVar ":RtVariables:CsrActiveConfig" $1
+}
+
+while getopts uh option; do
+    case $option in
+        u)
+            update=1
+        ;;
+        h)
+            showOptions
+            exit 0
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [[ ! -e $1 ]]; then showOptions; exit 1; fi
+
+if [[ $update ]]; then
+    updateConfig $1
+else
+    replaceConfig $1
+fi
