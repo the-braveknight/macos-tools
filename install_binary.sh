3279498549e5b721e6b97ab30cf92145da0467e4
Zaid Rahawi <zaid.riadh@gmail.com>
2018-02-05 01:50:38 +0300

    Add option to install kexts to CLOVER/kexts/Other

--- install_binary.sh
+++ install_binary.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
-
 function showOptions() {
     echo "-d,  Directory to install all binaries within."
     echo "-h,  Show this help message."
--- install_kext.sh
+++ install_kext.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+DIR=$(dirname $0)
+
+kexts_dir=/Library/Extensions
+
 function showOptions() {
     echo "-d,  Directory to install all kexts within."
+    echo "-i,  Install kext(s) to EFI/CLOVER/kexts/Other."
     echo "-e,  Kexts exceptions (single string) when installing multiple kexts."
     echo "-h,  Show this help message."
     echo "Usage: $(basename $0) [-e <Kext exceptions>] [Kext(s) to install]"
@@ -10,22 +15,24 @@ function showOptions() {
 
 function installKext() {
     kextName=$(basename $1)
-    echo Installing $kextName to /Library/Extensions
-    sudo rm -Rf /Library/Extensions/$kextName
-    sudo cp -Rf $1 /Library/Extensions
+    echo Installing $kextName to $kexts_dir
+    sudo rm -Rf $kexts_dir/$kextName
+    sudo cp -Rf $1 $kexts_dir
 }
-
-
 
 function check() {
     kextName=$(basename $1)
-    if [[ -z $exceptions || $(echo $1 | grep -vE $exceptions) ]]; then echo 1; fi
+    if [[ $(echo $1 | grep -vE "$exceptions") ]]; then echo 1; fi
 }
 
-while getopts e:d:h option; do
+while getopts e:id:h option; do
         case $option in
             e)
                 exceptions=$OPTARG
+            ;;
+            i)
+                EFI=$($DIR/mount_efi.sh)
+                kexts_dir=$EFI/EFI/Clover/kexts/Other
             ;;
             d)
                 directory=$OPTARG
