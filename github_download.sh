b806c703380dc9981a810b08187b1c6f1c9302ae
the-braveknight <zaid.riadh@gmail.com>
2018-04-13 23:08:54 +0300

    Rewrite github_download.sh from scratch

--- github_download.sh
+++ github_download.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-curl_options="--retry 5 --location --progress-bar"
-curl_options_silent="--retry 5 --location --silent"
-
 function showOptions() {
-    echo "-r,  GitHub repo for project."
-    echo "-o,  Output file to save as."
+    echo "-u,  GitHub user name."
+    echo "-r,  GitHub repo name."
+    echo "-f,  Partial file name to look for."
+    echo "-o,  Output directory."
     echo "-h,  Show this help menu."
-    echo "Usage: $(basename $0) [-r <GitHub repo release page URL>] [-o <output filename>]"
-#    echo "Example: $(basename $0) -a RehabMan -n os-x-fakesmc-kozlek -o ~/Downloads"
+    echo "Usage: $(basename $0) [-u <GitHub user name>] [-r <GitHub repo name>] [-f <File name to look for>] [-o <Output directory>]"
+    echo "Example: $(basename $0) -u vit9696 -r Lilu -f RELEASE -o ~/Downloads"
 }
 
-# download latest release from github (perhaps others)
-# by RehabMan
-function download_latest()
-# $1 is main URL
-# $2 is URL of release page
-# $3 is partial file name to look for
-# $4 is file name to rename to
-{
-    echo "Downloading $(basename $4) from $2"
-    curl $curl_options_silent --output /tmp/org.rehabman.download.txt "$2"
-    local scrape=`grep -o -m 1 "/.*$3.*\.zip" /tmp/org.rehabman.download.txt`
-    local url=$1$scrape
-    echo $url
-    curl $curl_options --output "$4" "$url"
-    echo
+function download() {
+# $1: GitHub user (author) name
+# $2: GitHub repo name
+# $3: Partial file name to look for ("RELEASE", "DEBUG", etc.)
+# $4: Output directory
+    curl --silent --location "https://github.com/$1/$2/releases" --output "/tmp/org.$1.download.txt"
+    scrape=$(grep -o -m 1 "/.*$3.*\.zip" "/tmp/org.$1.download.txt")
+    fileName="$1-$2.zip"
+    echo Downloading $fileName
+    curl --progress-bar --location https://github.com/$scrape --output $4/$fileName
 }
 
 if [[ ! -n $@ ]]; then showOptions; exit 1; fi
 
-while getopts r:n:o:h option; do
+while getopts u:r:f:o:h option; do
     case $option in
-    r)
-        release_url=$OPTARG
+    u)
+        github_user=$OPTARG
     ;;
-    n)
-        name=$OPTARG
+    r)
+        github_repo=$OPTARG
+    ;;
+    f)
+        partial_name=$OPTARG
     ;;
     o)
-        output_filename=$OPTARG
+        output_directory=$OPTARG
     ;;
     h)
         showOptions
@@ -50,11 +47,13 @@ done
 
 shift $((OPTIND-1))
 
-if [[ ! -e $outputDirectory ]]; then outputDirectory=.; fi
+if [[ ! -e $output_directory ]]; then output_directory=.; fi
 
-if [[ ! -n $release_url || ! -n $output_filename ]]; then
+if [[ ! -n $partial_name ]]; then partial_name=RELEASE; fi
+
+if [[ ! -n $github_user || ! -n $github_repo ]]; then
     showOptions
     exit 1
 fi
 
-download_latest "https://github.com" $release_url "RELEASE" $output_filename
+download $github_user $github_repo $partial_name $output_directory
