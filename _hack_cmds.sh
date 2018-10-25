c5d0640290952e27148d24290a6d2adefb0b6287
the-braveknight <zaid.riadh@gmail.com>
2018-10-25 13:36:49 +0300

    Bug fixes; refactoring

--- _hack_cmds.sh
+++ _hack_cmds.sh
@@ -13,7 +13,6 @@ source $tools_dir/_lilu_helper.sh
 downloads_dir=$repo_dir/Downloads
 hotpatch_dir=$repo_dir/Hotpatch/Downloads
 local_kexts_dir=$repo_dir/Kexts
-build_dir=$repo_dir/Build
 
 if [[ -z "$repo_plist" ]]; then
     if [[ -e "$repo_dir/repo_config.plist" ]]; then
@@ -32,25 +31,6 @@ if [[ -z "$config_plist" ]]; then
         exit 2
     fi
 fi
-
-function installed() {
-# $1: Can be either 'Kexts', 'Apps', or 'Tools'.
-    printInstalledItems "$1"
-}
-
-function deprecated() {
-# $1: Can be either 'Kexts', 'Apps', or 'Tools'.
-    printArrayItems "Deprecated:$1" "$repo_plist"
-}
-
-function installation() {
-# $1: Can be either 'Kexts', 'Apps', or 'Tools'.
-    for ((index=0; 1; index++)); do
-        name=$(printValue "Installations:$1:$index:Name" "$repo_plist" 2> /dev/null)
-        if [[ $? -ne 0 ]]; then break; fi
-        echo "$name"
-    done
-}
 
 case "$1" in
     --download-requirements)
@@ -82,28 +62,35 @@ case "$1" in
     ;;
     --install-apps)
         unarchiveAllInDirectory "$downloads_dir"
-        for app in $(installation "Apps"); do
-            installApp $(findApp "$app" "$downloads_dir")
+        for ((index=0; 1; index++)); do
+            name=$(printValue "Installations:Apps:$index:Name" "$repo_plist" 2> /dev/null)
+            if [[ $? -ne 0 ]]; then break; fi
+            app=$(findApp "$name" "$downloads_dir")
+            if [[ -e "$app" ]]; then
+                installApp "$app"
+            fi
         done
     ;;
     --install-tools)
         unarchiveAllInDirectory "$downloads_dir"
-        for tool in $(installation "Tools"); do
-            installTool $(findTool "$tool" "$downloads_dir")
+        for ((index=0; 1; index++)); do
+            name=$(printValue "Installations:Tools:$index:Name" "$repo_plist" 2> /dev/null)
+            if [[ $? -ne 0 ]]; then break; fi
+            tool=$(findTool "$name" "$downloads_dir")
+            if [[ -e "$tool" ]]; then
+                installTool "$tool"
+            fi
         done
-    ;;
-    --build-kexts)
-        if [[ ! -d "$build_dir" ]]; then mkdir $build_dir; fi
-        hda_codec=$(printValue "Codec" "$repo_plist" 2> /dev/null)
-        if [[ -n "$hda_codec" ]]; then
-            createHDAInjector "$hda_codec" "Resources_$hda_codec" "$build_dir"
-        fi
-        createLiluHelper "$build_dir"
     ;;
     --install-kexts)
         unarchiveAllInDirectory "$downloads_dir"
-        for kext in $(installation "Kexts"); do
-            installKext $(findKext "$kext" "$downloads_dir" "$build_dir" "$local_kexts_dir")
+        for ((index=0; 1; index++)); do
+            name=$(printValue "Installations:Kexts:$index:Name" "$repo_plist" 2> /dev/null)
+            if [[ $? -ne 0 ]]; then break; fi
+            kext=$(findKext "$name" "$downloads_dir" "$local_kexts_dir")
+            if [[ -e "$kext" ]]; then
+                installKext "$kext"
+            fi
         done
     ;;
     --install-essential-kexts)
@@ -115,29 +102,29 @@ case "$1" in
             name=$(printValue "Installations:Kexts:$index:Name" "$repo_plist" 2> /dev/null)
             if [[ $? -ne 0 ]]; then break; fi
             essential=$(printValue "Installations:Kexts:$index:Essential" "$repo_plist" 2> /dev/null)
-            if [[ "$essential" == "true" ]]; then
-                installKext $(findKext "$name" "$downloads_dir" "$build_dir" "$local_kexts_dir") "$efi_kexts_dest"
+            kext=$(findKext "$name" "$downloads_dir" "$local_kexts_dir")
+            if [[ -e "$kext" && "$essential" == "true" ]]; then
+                installKext "$kext" "$efi_kexts_dest"
             fi
         done
     ;;
     --remove-installed-kexts)
-        for kext in $(installed "Kexts"); do
+        for kext in $(printInstalledItems "Kexts"); do
             removeKext "$kext"
         done
     ;;
     --remove-installed-apps)
-        for app in $(installed "Apps"); do
+        for app in $(printInstalledItems "Apps"); do
             removeApp "$app"
         done
     ;;
     --remove-installed-tools)
-        for tool in $(installed "Tools"); do
+        for tool in $(printInstalledItems "Tools"); do
             removeTool "$tool"
         done
     ;;
     --remove-deprecated-kexts)
-        # Remove deprecated kexts
-        for kext in $(deprecated "Kexts"); do
+        for kext in $(printArrayItems "Deprecated:Kexts" "$repo_plist"); do
             removeKext "$kext"
         done
     ;;
@@ -160,7 +147,6 @@ case "$1" in
         $0 --install-tools
         $0 --install-apps
         $0 --remove-deprecated-kexts
-        $0 --build-kexts
         $0 --install-essential-kexts
         $0 --install-kexts
         $0 --update-kernelcache
